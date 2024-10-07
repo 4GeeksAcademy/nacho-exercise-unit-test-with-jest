29e10cf1f50e3f5a727bccb78231b1d7b5183512
José Ignacio Puig <ipnachoip@gmail.com>
2024-10-07 12:25:53 +0000

    reenvio de proyecto

--- test.js
+++ test.js
@@ -14,10 +14,10 @@ test("One euro should be 1.07 dollars", function() {
     //const dollars = fromEuroToDollar(3.5);
 
     // Si 1 euro son 1.07 dólares, entonces 3.5 euros debe ser (3.5 * 1.07)
-    const expected = 3.5 * 1.07;
+    const expected = 125 * 1.07;
 
     // Hago mi comparación (la prueba)
-    expect(fromEuroToDollar(3.5)).toBe(3.745); // 1 euro son 1.07 dólares, entonces 3.5 euros deberían ser = (3.5 * 1.07)
+    expect(fromEuroToDollar(125)).toBe(133.75); 
 })
 
 test("One yen should be x dollars", function() {
@@ -27,9 +27,23 @@ test("One yen should be x dollars", function() {
     // Uso la función como debe ser usada
     //const yen = fromDollarToYen(3.5);
 
-    // Si 156.5 yenes son 1.07 dólares, entonces 3.5 yenes deben ser (3.5 * 1.07)
+    
     const expected = 3.5 * 1.07;
 
     // Hago mi comparación (la prueba)
-    expect(fromEuroToDollar(3.5)).toBe(3.745); // 1 euro son 1.07 dólares, entonces 3.5 euros deberían ser = (3.5 * 1.07)
+    expect(fromDollarToYen(3.5)).toBe(511.91588785046724); 
+})
+
+test("One yen should be x pound", function() {
+    // Importo la funcion desde app.js
+    const { fromYenToPound } = require('./app.js');
+
+    // Uso la función como debe ser usada
+    //const yen = fromDollarToYen(3.5);
+
+    
+    const expected = 3.5 * 0.87;
+
+    // Hago mi comparación (la prueba)
+    expect(fromYenToPound(3.5)).toBe(0.019456869009584665); 
 })
